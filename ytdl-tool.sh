b3ad900faa895ecce49f9ad97d53e3c1316f14e1
crimera <44558091+crimera@users.noreply.github.com>
2021-03-09 23:25:42 +0800

    update ytdl-tool.sh

--- ytdl-tool.sh
+++ ytdl-tool.sh
@@ -4,7 +4,7 @@ while [ -n "$1" ]; do # while loop starts
 
 	-clip)
 	    test -z $url
-	    if [ $? == 0 ]
+	    if (( $? == 0))
 	    then
 	    url=$2
         ffmpeg -ss $3 -to $4 -i "$(youtube-dl -f best --get-url $2)" -c:v copy -c:a copy $5.mp4
@@ -16,7 +16,7 @@ while [ -n "$1" ]; do # while loop starts
 	-title)
 	    test -z $url
 	    echo $?
-	    if [ $? == 0 ]
+	    if (($? == 0))
 	    then
 	    url=$2
 		youtube-dl --get-title $2
