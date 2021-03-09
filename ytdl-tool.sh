786790a1071742f6e5a497f070a7fdcd7559d5ac
crimera <44558091+crimera@users.noreply.github.com>
2021-03-09 22:13:50 +0800

    update ytdl-tool.sh

--- ytdl-tool.sh
+++ ytdl-tool.sh
@@ -22,6 +22,6 @@ read end_secs
 from=$start_hrs:$start_mins:start_secs
 to=$end_hrs:end_mins:end_secs
 
-ffmpeg -i "$(youtube-dl -f best --get-url $url_test)" -ss $from -to $to -c:v copy -c:a copy out.mp3
+ffmpeg -ss $from -to $to -i "$(youtube-dl -f best --get-url $url_test)" -c:v copy -c:a copy out.mp3
 
 # ffmpeg -i $(youtube-dl -f best --get-url 'https://youtu.be/RUecxcEGyCM') -ss 00:00:00 -to 00:00:10 -c:v copy -c:a copy out3.mp4
